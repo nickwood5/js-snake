b65705cbf3f262739a4c384d1981e9dcf6e51aa4
Nicholas Wood <horizon.nicholas14@gmail.com>
2022-02-24 02:01:11 -0500

    Added snake continuity along game board borders

--- input.js
+++ input.js
@@ -19,26 +19,30 @@ function getInput(player, press) {
     if (player.inputBuffer.length < maxInputs) {
         switch (press.key) {
             case player.controls.up:
-                if (player.currentDirection.y !== 0) break
-                player.currentDirection = { x : 0, y : -1}
+                if (player.lastDirection.y !== 0) break
+                console.log("Player move up")
+                player.lastDirection = { x : 0, y : -1}
                 player.inputBuffer.push({ x : 0, y : -1})
                 break 
             
             case player.controls.down:
-                if (player.currentDirection.y !== 0) break
-                player.currentDirection = { x : 0, y : 1}
+                if (player.lastDirection.y !== 0) break
+                console.log("Player move down")
+                player.lastDirection = { x : 0, y : 1}
                 player.inputBuffer.push({ x : 0, y : 1})
                 break
             
             case player.controls.left:
-                if (player.currentDirection.x !== 0) break
-                player.currentDirection = { x : -1, y : 0}
+                if (player.lastDirection.x !== 0) break
+                console.log("Player move left")
+                player.lastDirection = { x : -1, y : 0}
                 player.inputBuffer.push({ x : -1, y : 0})
                 break
             
             case player.controls.right:
-                if (player.currentDirection.x !== 0) break
-                player.currentDirection = { x : 1, y : 0}
+                if (player.lastDirection.x !== 0) break
+                console.log("Player move right")
+                player.lastDirection = { x : 1, y : 0}
                 player.inputBuffer.push({ x : 1, y : 0})
                 break
         }
--- snake.js
+++ snake.js
@@ -3,21 +3,25 @@ import { inputBuffer } from "./input.js"
 
 
 class player {
-    constructor(colour, nodes, controls) {
-        this.colour = colour
+    constructor(name, nodes, controls) {
+        this.name = name
         this.nodes = nodes
         this.currentDirection = {x: 0, y: 0}
+        this.lastDirection = {x: 0, y: 0}
         this.inputBuffer = [{ x : 0, y : 0}]
         this.controls = controls
         this.addedNodes = 0;
     }
 }
 
+
+
 class fruit {
     constructor(colour) {
         this.colour = colour
-        this.x = Math.floor(Math.random() * 20)
-        this.y = Math.floor(Math.random() * 20)
+        this.x = Math.floor(Math.random() * 21) + 1
+        this.y = Math.floor(Math.random() * 21) + 1
+        console.log("New fruit at " + this.x + ", " + this.y)
     }
 }
 
@@ -29,17 +33,17 @@ export let playerOne = new player("red", [{ x: 11, y: 11 }, { x: 11, y: 12 }, { 
 export let playerTwo = new player("blue", [{ x: 5, y: 11 }, { x: 5, y: 12 }, { x: 5, y: 13 }], wasdControls)
 
 let fruitOne = new fruit("blue")
-let fruitTwo = new fruit("blue")
-let fruits = [fruitOne]
+let fruitTwo = new fruit("red")
+let fruits = [fruitOne, fruitTwo]
 
 
-export let allPlayers = [playerOne, playerTwo]
+export let allPlayers = [playerOne]
 
 const gameBoard = document.getElementById('game-board')
 const snakeNodes = [{ x: 11, y: 11 }, { x: 11, y: 12 }, { x: 11, y: 13 }]
 
 
-const SNAKE_SPEED = 20
+const SNAKE_SPEED = 10
 let snakeHead = { x: 11, y: 11}
 let currentDirection = { x: 0, y: 0}
 let lastRenderTime = 0
@@ -64,50 +68,99 @@ function updateAll(allPlayers) {
     })
 }
 
-function update(snake, fruits, allPlayers) {
+function killPlayer(allPlayers, removedName) {
+    for (let i = 0; i < allPlayers.length; i++) {
+        let player = allPlayers[i]
+        if (player.name == removedName) {
+            allPlayers.splice(i, 1)
+            return
+        }
+    }
+}
+
+function deleteFruit(fruits, fruitX, fruitY) {
     fruits.forEach(f => {
-        if (f.x == snake.nodes[0].x + snake.currentDirection.x && f.y == snake.nodes[0].y + snake.currentDirection.y) {
-            fruits.pop()
+
+    })
+}
+
+
+function checkFruitEat(player, fruits, nextPos) {
+    for (let i = 0; i < fruits.length; i++) {
+        let f = fruits[i]
+        if (f.x == nextPos.x && f.y == nextPos.y) {
+            console.log("Eat fruit")
+            fruits.splice(i, 1)
             let newFruit = new fruit("aa")
             fruits.push(newFruit)
-            snake.addedNodes += 3
-        }
-    })
+            player.addedNodes += 3
+            return
+        } 
+    }
+}
 
-    allPlayers.forEach(player => {
-        player.nodes.forEach(n => {
-            if (n.x == snake.nodes[0].x + snake.currentDirection.x && n.y == snake.nodes[0].y + snake.currentDirection.y && (snake.currentDirection.x !== 0 || snake.currentDirection.y !== 0)) {
-                allPlayers.pop()
+function checkCollisions(player, allPlayers, nextPos) {
+    allPlayers.forEach(p => {
+        p.nodes.forEach(n => {
+            if (n.x == nextPos.x && n.y == nextPos.y) {
+                killPlayer(allPlayers, player.name)
             }
         })
     })
+}
 
-    if (snake.inputBuffer.length > 0) {
+function updateDirection(player) {
+    if (player.inputBuffer.length > 0) {
         console.log("Execute command")
-        console.log(snake.inputBuffer[0])
-        snake.currentDirection = snake.inputBuffer[0]
-        snake.inputBuffer.shift()
+        console.log(player.inputBuffer[0])
+        player.currentDirection = player.inputBuffer[0]
+        player.inputBuffer.shift()
     }
+}
 
-    if (snake.currentDirection.x !== 0 || snake.currentDirection.y !== 0) {
-        snakeHead = snake.nodes[0]
-
-        snake.nodes.unshift({ x : snakeHead.x + snake.currentDirection.x, y : snakeHead.y + snake.currentDirection.y })
-        
-
-        if (snake.addedNodes == 0) {
-            snake.nodes.pop()
+function grow(player) {
+    if (player.currentDirection.x !== 0 || player.currentDirection.y !== 0) {
+        if (player.addedNodes == 0) {
+            console.log("DDD")
+            player.nodes.pop()
         } else {
-            snake.addedNodes -= 1
+            player.addedNodes -= 1
         }
     }
+}
 
-    console.log("Printing snake")
-    console.log(snake.nodes.length)
-    for (let i = 0; i < snake.nodes.length; i++) {
-        console.log(snake.nodes[i])
+function getNextPos(player) {
+    snakeHead = player.nodes[0]
+
+    if (snakeHead.y == 1 && player.currentDirection.y == -1) {
+        return { x : snakeHead.x + player.currentDirection.x, y : 21 }
+    } 
+
+    if (snakeHead.y == 21 && player.currentDirection.y == 1) {
+        return { x : snakeHead.x + player.currentDirection.x, y : 1 }
+    } 
+
+    if (snakeHead.x == 21 && player.currentDirection.x == 1) {
+        return { x : 1, y : snakeHead.y + player.currentDirection.y }
+    } 
+        
+    if (snakeHead.x == 1 && player.currentDirection.x == -1) {
+        return { x : 21, y : snakeHead.y + player.currentDirection.y }
+    } 
+        
+    return { x : snakeHead.x + player.currentDirection.x, y : snakeHead.y + player.currentDirection.y }
+}
+
+function update(snake, fruits, allPlayers) {
+    updateDirection(snake)
+    if (snake.currentDirection.x !== 0 || snake.currentDirection.y !== 0) {
+        let nextPos = getNextPos(snake)
+        checkFruitEat(snake, fruits, nextPos)
+        checkCollisions(snake, allPlayers, nextPos)
+        grow(snake)
+        console.log(nextPos)
+        snake.nodes.unshift(nextPos)
     }
-    
 }
 
 
@@ -119,13 +172,21 @@ function draw(allPlayers, gameBoard) {
 }
 
 function drawSnake(gameBoard, snake) {
-    snake.nodes.forEach(node => {
-      const snakeNode = document.createElement('div')
-      snakeNode.style.gridRowStart = node.y
-      snakeNode.style.gridColumnStart = node.x
-      snakeNode.classList.add('snakeNode')
-      gameBoard.appendChild(snakeNode)
-    })
+    let node = snake.nodes[0]
+    const snakeNode = document.createElement('div')
+    snakeNode.style.gridRowStart = node.y
+    snakeNode.style.gridColumnStart = node.x
+    snakeNode.classList.add('snakeHead')
+    gameBoard.appendChild(snakeNode)
+
+    for (let i = 1; i < snake.nodes.length; i++) {
+        let node = snake.nodes[i]
+        const snakeNode = document.createElement('div')
+        snakeNode.style.gridRowStart = node.y
+        snakeNode.style.gridColumnStart = node.x
+        snakeNode.classList.add('snakeNode')
+        gameBoard.appendChild(snakeNode)
+    }
 }
 
 function drawFruit(gameBoard, fruits) {
